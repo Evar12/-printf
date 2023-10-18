daa1c8f3c69a13f787d402bd07e8f9f8c5811154
ElizabethChidinma <echidinma68@gmail.com>
2023-10-18 13:11:02 -0700

    updated

--- get_int.c
+++ get_int.c
@@ -38,26 +38,26 @@ int _int(va_list integ)
  */
 int _dec(va_list decimal)
 {
-        int a, expo = 1, len = 0;
-        unsigned int n;
+	int a, expo = 1, len = 0;
+	unsigned int n;
 
-        a = va_arg(decimal, int);
+	a = va_arg(decimal, int);
 
-        if (a < 0)
-        {
-                len = len + _putchar('-');
-                n = a * -1;
-        }
-        else
-                n = a;
-        while (n / expo > 9)
-                expo *= 10;
+	if (a < 0)
+	{
+		len = len + _putchar('-');
+		n = a * -1;
+	}
+	else
+		n = a;
+	while (n / expo > 9)
+		expo *= 10;
 
-        while (expo != 0)
-        {
-                len = len + _putchar(n / expo + '0');
-                n = n % expo;
-                expo = expo / 10;
-        }
-        return (len);
+	while (expo != 0)
+	{
+		len = len + _putchar(n / expo + '0');
+		n = n % expo;
+		expo = expo / 10;
+	}
+	return (len);
 }
